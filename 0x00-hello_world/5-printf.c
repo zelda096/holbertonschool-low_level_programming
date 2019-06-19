615f93771bf9d08ccfc366267dd3a4da362d7f7d
zelda096 <966@holbertonschool.com>
2019-06-19 19:58:05 +0000

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
-* main - Entry point
-*
-* Return: A text
-*/
+ * main - Entry point
+ *
+ * Return: A text
+ */
+
 int main(void)
 {
-printf("$s\n", "with proper grammar, but the outcome is a piece of art,");
-return (0);
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
