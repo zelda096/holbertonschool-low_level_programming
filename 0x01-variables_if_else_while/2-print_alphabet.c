2e101daf8d540f04901f5bfccde6e97c3007f89d
zelda096 <966@holbertonschool.com>
2019-06-20 20:05:43 +0000

    print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: A text
+*/
+int main(void)
+{
+	int a = 'a';
+
+	while (a <= 'z')
+	{
+		putchar('a');
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
