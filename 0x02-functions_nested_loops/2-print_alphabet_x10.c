039d89f230f77fbafc9de82f6c07d02dd81ea8b5
zelda096 <966@holbertonschool.com>
2019-06-25 17:59:22 +0000

    10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,21 @@
 #include "holberton.h"
 /**
-* print_alphabet - entry point
+* print_alphabet_x10 - entry point
 * Des: print the alphabet with _putchar
 * Return: A text
 */
+
 void print_alphabet_x10(void)
 {
-	int x = 0;
+	int x;
+	char y;
 
-	int y = 'a';
-
+	for (x = 0 ; x < 10; x++)
+	{
+		for (y = 'a' ; y <= 'z'; y++)
+		{
+			_putchar(y);
+		}
+		_putchar('\n');
+	}
 }
