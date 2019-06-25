ba143d5c3fdb0660b131ff7fec7eafe33bcdce91
zelda096 <966@holbertonschool.com>
2019-06-25 15:47:16 +0000

    putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+* main - entry point
+*
+* Return: A text
+*/
+int main(void)
+{
+	char a[9] = "Holberton";
+
+	int x = 0;
+
+	while (x <= 9)
+	{
+		_putchar(a[x]);
+		x++;
+
+	}
+
+	_putchar('\n');
+	return (0);
+
+}
