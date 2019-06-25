8424522abf6224b82860d163a7d663b87a5c0ae5
zelda096 <966@holbertonschool.com>
2019-06-25 20:02:02 +0000

    hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+* jack_bauer - entry point
+* Des: min of jack
+* Return: 0
+*/
+void jack_bauer(void)
+{
+	int x, y;
+
+	for (x = 0; x < 24; x++)
+	{
+		for (y = 0; y < 60; y++)
+		{
+			_putchar(x / 10 + '0');
+			_putchar(x % 10 + '0');
+			_putchar(':');
+			_putchar(y / 10 + '0');
+			_putchar(y % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
