79a483aef0155dc53f81b7887f4ca8c6d01a7726
zelda096 <966@holbertonschool.com>
2019-06-27 19:21:15 +0000

    numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,9 +7,11 @@
  */
 void more_numbers(void)
 {
-	for (int x = 0; x < 10; x++)
+	int x, y;
+
+	for (x = 0; x < 10; x++)
 	{
-		for (int y = 0; y < 15; y++)
+		for (y = 0; y <= 14; y++)
 		{
 			if (y > 9)
 			{
