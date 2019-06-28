9f6278a5654444c5ee1723a52fb2440f5cc5e48c
zelda096 <966@holbertonschool.com>
2019-06-28 00:10:43 +0000

    task11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+/**
+ * main - entry point
+ * 
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int x, y;
+	long int z = 0;
+	long int bers = 30;
+	long int d = num;
+
+	for (x = 2; x <= bers ; x++)
+	{
+		for (y = 1; y <= x; y++)
+		{
+			if (x % y == 0)
+				z++;
+		}
+		if ((z == 2) && (bers % x == 0))
+		{
+			d = bers / x;
+			if (d == 1)
+			{
+				printf("%ld", x);
+				break;
+			}
+		}
+		z = 0;
+	}
+	printf("\n");
+	return (0);
+}
