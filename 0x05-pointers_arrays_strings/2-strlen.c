175c72f142a36356aa142414ef7e62eaafd2ab59
zelda096 <966@holbertonschool.com>
2019-07-02 17:37:26 +0000

    string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * _strlen - strings.
+ *
+ * @s: var pointer
+ *
+ * Return: in begin
+ */
+int _strlen(char *s)
+{
+	int begin = 0;
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		if (s[x] == '\0')
+			break;
+
+		begin++;
+	}
+	return (begin);
+}
