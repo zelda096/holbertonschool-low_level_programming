1af40b5239fab4892bb2e75658cf01ba739f2794
zelda096 <966@holbertonschool.com>
2019-07-02 15:19:05 +0000

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - pointer.
+ * Des: updates the value it points to to 98.
+ * @n: var pointer
+ *
+ * Return in 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
