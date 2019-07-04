906d78ef415771ce8ee2bacb50d74e918b6c41e5
zelda096 <966@holbertonschool.com>
2019-07-04 19:49:16 +0000

    task3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,9 +11,9 @@ int _strcmp(char *s1, char *s2)
 	int total;
 
 	while (s1[x] == s2[x] && (s1[x] != '\0' || s2[x] != '\0'))
-		{
-			x++;
-		}
+	{
+		x++;
+	}
 	total = s1[x] - s2[x];
 	return (total);
 }
