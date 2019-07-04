df360a66aeb486d6f5ace7a04f5f0cb9f304f51b
zelda096 <966@holbertonschool.com>
2019-07-04 19:15:37 +0000

    task2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,17 +8,15 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int x = 0;
+	int x;
 
-	while (src[x] != '\0' && x < n)
+	for (x = 0; x < n && src[x] != '\0'; x++)
 	{
-		x++;
+		dest[x] = src[x];
 	}
-	dest[x] = src[x];
 	while (x < n)
 	{
-		x++;
+		dest[x++] = '\0';
 	}
-	dest[x] = '\0';
 	return (dest);
 }
