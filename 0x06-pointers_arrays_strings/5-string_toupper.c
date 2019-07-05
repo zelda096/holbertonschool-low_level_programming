29338db3743c4ae004dd2f7f296036c8c295ed2f
zelda096 <miguelangel697@gmail.com>
2019-07-04 22:28:15 -0500

    5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *string_toupper - function for string
+ *@s: pointer variable
+ *Return: s
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+	}
+	return (s);
+}
