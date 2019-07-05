279e70a876d4ab4862a9416d729b9e91aec414e8
zelda096 <miguelangel697@gmail.com>
2019-07-04 22:50:03 -0500

    8

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,22 +1,22 @@
 #include "holberton.h"
 /**
- * reverse_array - function for rev
- *@a: pointer variable
- *@n: parameter
- *Return: 0
+ * reverse_array - rev. the order of an array
+ * @a : pointer-parameter
+ * @n : parameter
+ * Return: ntg.
  */
 void reverse_array(int *a, int n)
 {
-	int x = 0;
-	int y = n - 1;
+	int i = 0;
+	int j = n - 1;
 	int tmp;
 
 	while (i < j)
 	{
-		tmp = a[x];
-		a[x] = a[y];
-		a[y] = tmp;
-		x++;
-		y--;
+		tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+		i++;
+		j--;
 	}
 }
