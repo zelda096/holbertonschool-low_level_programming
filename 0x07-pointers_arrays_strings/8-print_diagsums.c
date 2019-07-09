a9e3b1bef2f77355096d18232b701f48e7ffcdc0
zelda096 <miguelangel697@gmail.com>
2019-07-08 22:24:32 -0500

    task8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_diagsums - entry point
+ * Description: print sum 2 diagonals (square matrix)
+ * @a: pointer variable
+ * @size: sizeof matrix
+ * Return: 0
+ */
+void print_diagsums(int *a, int size)
+{
+	int x;
+	int dsum = 0;
+	int usum = 0;
+
+	for (x = 0; x < size; x++)
+	{
+		dsum += *(a + ((size * x) + x));
+		usum += *(a + (size - 1) * (x + 1));
+	}
+	printf("%d, %d\n", dsum, usum);
+}
