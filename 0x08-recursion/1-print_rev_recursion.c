4d50b14755738b6550076adf1e32e99629805783
zelda096 <miguelangel697@gmail.com>
2019-07-10 18:31:59 -0500

    task1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _print_rev_recursion -  prints a string in reverse.
+ * @s: string
+ * Return: ntg
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		s++;
+		_print_rev_recursion(s);
+		_putchar(*(s - 1));
+	}
+}
