9ffc542641541a278e534a08b163ee7fbfdd1499
zelda096 <miguelangel697@gmail.com>
2019-07-10 18:38:45 -0500

    task3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * factorial - entry point
+ * @n: int
+ * Return: 0 or -1
+*/
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	return (n * factorial(n - 1));
+}
