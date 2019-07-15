d4f6a745ebaad75005d9e8a89160104368d4b5ac
zelda096 <miguelangel697@gmail.com>
2019-07-14 19:22:14 -0500

    name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: function name
+ * @argv: array with arg
+ * Return: function name
+*/
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
