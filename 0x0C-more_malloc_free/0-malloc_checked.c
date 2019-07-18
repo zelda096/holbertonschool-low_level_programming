9e9abf6d6711809601fd4da80409b2a4830e12af
zelda096 <miguelangel697@gmail.com>
2019-07-18 18:00:12 -0500

    task0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+
+#include "holberton.h"
+#include <stdlib.h>
+/**
+* malloc_checked - entry point
+* @b: size of mem.
+* Return: pointer to mem, failure = 98.
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *mem;
+
+	mem = malloc(b);
+	if (mem == NULL)
+	{
+		free(memy);
+		exit(98);
+	}
+	return (mem);
+}
