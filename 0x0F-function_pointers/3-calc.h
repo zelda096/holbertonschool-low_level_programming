4546a26d36d2be7898ed7dca9417b6af279ad44a
zelda096 <966@holbertonschool.com>
2019-07-24 19:39:59 +0000

    struct for operator

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,25 @@
+#ifndef CALCULATOR_OPERATOR
+#define CALCULATOR_OPERATOR
+/**
+ * struct op - structure for operator
+ * @op: The operator
+ * @f: The function associated
+ */
+
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+
+int op_add(int a, int b);
+
+int op_sub(int a, int b);
+
+int op_mul(int a, int b);
+
+int op_div(int a, int b);
+
+int op_mod(int a, int b);
+
+#endif /**calculator*/
