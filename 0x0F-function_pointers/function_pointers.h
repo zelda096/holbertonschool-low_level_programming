e45b0901ede63fb71a3b4b4afa0726762193b11c
zelda096 <966@holbertonschool.com>
2019-07-24 18:08:56 +0000

    header

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,6 @@
+#efndif FUNCTION_TO_POINTER
+#define FUNCTION_TO_POINTER
+
 #include <stdio.h>
 
 int _putchar(char c);
@@ -5,3 +8,5 @@ int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 
 void array_iterator(int *array, size_t size, void (*action)(int));
+
+#endif
