19ccabb48444458e06d318c237a375704842c9df
zelda096 <966@holbertonschool.com>
2019-08-01 23:41:53 +0000

    update

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef _LINK_LIST_
 #define _LINK_LIST_
 
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -23,5 +24,7 @@ typedef struct list_s
 int _putchar(char c);
 
 size_t print_list(const list_t *h);
+
+size_t list_len(const list_t *h);
 
 #endif
