afd672593cb5d3c9ea773e31d6ec7a5b57cfdafc
zelda096 <966@holbertonschool.com>
2019-08-13 23:32:08 +0000

    task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,21 +8,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int x;
+        int fd;
+        int x;
 
-	if (filename == NULL)
-		return (-1);
-	if (text_content == NULL)
-		text_content = "";
+        if (filename == NULL)
+                return (-1);
+        if (text_content == NULL)
+                text_content = "";
 
-	fd = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0600);
+        fd = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0600);
 
-	for (x = 0; text_content[x] != '\0'; x++)
-	{
-		if (write(fd, &text_content[x], 1) == -1)
-			return (-1);
-	}
-	close(fd);
-	return (1);
+        for (x = 0; text_content[x] != '\0'; x++)
+        {
+                if (write(fd, &text_content[x], 1) == -1)
+                        return (-1);
+        }
+        close(fd);
+        return (1);
 }
