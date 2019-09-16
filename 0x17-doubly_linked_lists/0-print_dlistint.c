d1194f0496cb721d94b5b3dd0873b75dc73935ed
zelda096 <miguelangel697@gmail.com>
2019-09-16 12:32:28 -0500

    task0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_dlistint - prints all elements of a doubly linked
+ * @h: pointer to node/head
+ * Return: node
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *cur = h;
+	size_t sz = 0;
+
+	while (cur)
+	{
+		printf("%i\n", cur->n);
+		cur = cur->next;
+		sz++;
+	}
+	return (sz);
+}
